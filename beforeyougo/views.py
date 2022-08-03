20079738680a25c718492f709edd0ecc860f7fbd
Emmacharleswilson <emmacharleswilson@googlemail.com>
2022-08-03 11:02:56 +0000

    removed before you go app

--- beforeyougo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def beforeyougo(request):
-    """ A view to return the index page """
-
-    return render(request, 'beforeyougo/beforeyougo.html')
