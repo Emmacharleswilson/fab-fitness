8126054cea0475f9afc33a942d97e88070881a32
Emmacharleswilson <emmacharleswilson@googlemail.com>
2022-08-05 15:26:45 +0000

    deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fab_fitness.wsgi:application
--- fab_fitness/settings.py
+++ fab_fitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wji1ojp55r(l5rqbb_jyy1)p@h7(lofb@42f#*-r(x-f3y(_ts
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fab-fitness.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'fab_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
